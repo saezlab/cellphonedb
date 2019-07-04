249410693337f3cfa93274802e7a6101127ce867
Türei Dénes <turei.denes@gmail.com>
2019-07-04 16:03:47 +0200

    omnipath module: workflow in class

--- omnipath/omnipath.py
+++ omnipath/omnipath.py
@@ -15,44 +15,63 @@
 import os
 import copy
 
-from pypath import main as pypath_main
-from pypath import data_formats
-from pypath import intercell
+from pypath import omnipath
 from pypath import cellphonedb
 from pypath import settings
 
 
-def main():
+class OmnipathCellphonedb(omnipath.OmniPath):
     
-    settings.setup(network_expand_complexes = False)
-
-    output_dir = os.path.join(
-        '..',
-        'cellphonedb',
-        'src',
-        'core',
-        'data',
-    )
-
-    network_pickle_path = 'omnipath_for_cellphonedb.pickle'
-
-    network = pypath_main.PyPath()
-
-    if os.path.exists(network_pickle_path):
-        
-        network.init_network(pfile = network_pickle_path)
-        
-    else:
-        
-        network_input = copy.deepcopy(data_formats.omnipath)
-        network_input.update(data_formats.ligand_receptor)
-        network_input.update(data_formats.ptm_misc)
-        network.load_omnipath(omnipath = network_input, remove_htp = False)
-
-    c = cellphonedb.CellPhoneDB(
-        network = network,
-        output_dir = output_dir,
-    )
-    c.main()
     
-    return c
+    def __init__(
+        self,
+        output_dir = None,
+        network_pickle = None,
+        annotation_pickle = None,
+        intercell_pickle = None,
+        complex_pickle = None,
+    ):
+        
+        settings.setup(network_expand_complexes = False)
+        
+        self.output_dir = (
+            output_dir or
+            os.path.join(
+                '..',
+                'cellphonedb',
+                'src',
+                'core',
+                'data',
+            )
+        )
+        
+        self.network_pickle = (
+            network_pickle or 'omnipath_for_cellphonedb.pickle'
+        )
+        self.intercell_pickle = (
+            intercell_pickle or 'omnipath_intercell.pickle'
+        )
+        self.annotation_pickle = (
+            annotation_pickle or 'omnipath_annot.pickle'
+        )
+        self.complex_pickle = (
+            complex_pickle or 'omnipath_complexes.pickle'
+        )
+        
+        omnipath.OmniPath.__init__(self)
+    
+    
+    def main(self):
+        
+        omnipath.OmniPath.main(self)
+        self.build()
+    
+    
+    def build(self):
+        
+        self.cellphonedb = cellphonedb.CellPhoneDB(
+            network = self.network,
+            annotation = self.intercell,
+            output_dir = output_dir,
+        )
+        self.cellphonedb.main()
